c7e171dfdd3232f1c2c5cd87a42deacbb02a7662
PeterPark1221 <137192346+PeterPark1221@users.noreply.github.com>
2025-07-09 23:18:24 +0900

    수정

--- Dynamic-Notch/Dynamic-Notch/Views/Tray/TrayFile.swift
+++ Dynamic-Notch/Dynamic-Notch/Views/Tray/TrayFile.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 //tray에 담은 파일을 정의
 // 파일의 이름 + 아이콘 + 파일확장자만 보이게 함
-struct TrayFile: Identifiable, Codable {
+struct TrayFile: Identifiable {
     let id: UUID
     let fileName: String
     let fileExtension: String
